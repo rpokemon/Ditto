fb656e2968f0ff6dc1bc35fc9627712394711360
Josh <josh.ja.butt@gmail.com>
2022-04-03 17:20:22 +1000

    Bump

--- ditto/__init__.py
+++ ditto/__init__.py
@@ -26,7 +26,7 @@ __title__ = "ditto"
 __author__ = "Bijij"
 __license__ = "MIT"
 __copyright__ = "Copyright 2018-present Bijij"
-__version__ = "2.0.3a"
+__version__ = "2.0.4a"
 
 from . import db as db, types as types, utils as utils
 from .config import *
