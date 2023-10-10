b260012cd664f1c3c484beed487cca4236d866ea
Zabbs <74313798+ItsZabbs@users.noreply.github.com>
2023-10-10 09:26:16 -0700

    Allow files to be logged along with embeds

--- ditto/utils/webhooks.py
+++ ditto/utils/webhooks.py
@@ -9,11 +9,13 @@ __all__ = ("EmbedWebhookLogger",)
 
 class EmbedWebhookLogger:
     _to_log: list[discord.Embed]
+    _files_to_log: list[discord.File]
 
     def __init__(self, webhool_url: str) -> None:
         self.loop = asyncio.get_event_loop()
         self._webhook_url = webhool_url
         self._to_log = []
+        self._files_to_log=[]
 
         self._session = aiohttp.ClientSession()
         self._webhook = discord.Webhook.from_url(self._webhook_url, session=self._session)
@@ -21,11 +23,14 @@ class EmbedWebhookLogger:
         # setup loop
         self._loop.start()
 
-    def log(self, embed: discord.Embed) -> None:
+    def log(self, embed: discord.Embed,files: list[discord.File]|None=None) -> None:
         self._to_log.append(embed)
+        if files is not None:
+            self._files_to_log.append(*files)
 
     @tasks.loop(seconds=5)
     async def _loop(self) -> None:
         while self._to_log:
             embeds = [self._to_log.pop(0) for _ in range(min(10, len(self._to_log)))]
-            await self._webhook.send(embeds=embeds)
+            files=[self._files_to_log.pop(0) for _ in range(min(10,len(self._files_to_log)))]
+            await self._webhook.send(embeds=embeds,files=files if files else [])
